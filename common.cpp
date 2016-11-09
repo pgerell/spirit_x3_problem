93cbc7fc910ee0f8947141d79b8336092717df59
Larry Evans <cppljevans@suddenlink.net>
2016-11-09 10:06:35 -0600

    Disabling BOOST_SPIRIT_INSTANTIATE enables link to
    -*- mode: compilation; default-directory: "~/prog_dev/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/" -*-
    Compilation started at Wed Nov  9 10:01:10
    
    make -f ../linkerr.mk MAIN=prog x -Wcommon.cpp
    install -d `dirname /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o`
    /home/evansl/dwnlds/llvm/3.8/prebuilt/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04/bin/clang++ -c -O0 -stdlib=libc++  -std=c++14 -ftemplate-backtrace-limit=0 -fdiagnostics-show-template-tree -fno-elide-type -fmacro-backtrace-limit=0     -I/home/evansl/prog_dev/boost/releases/ro/boost_1_61_0    -ftemplate-depth=200  common.cpp -MMD -o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o
    sed -e 's#^common.o:#/tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o:#' /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.d> /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o.dep
    rm /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.d
    /home/evansl/dwnlds/llvm/3.8/prebuilt/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04/bin/clang++ -stdlib=libc++    /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.o  -o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.exe
    
    Compilation finished at Wed Nov  9 10:01:12

--- common.cpp
+++ common.cpp
@@ -2,7 +2,7 @@
 #include "ast_adapted.hpp"
 #include "common_def.hpp"
 using start_decl=decltype(start);//mimic what BOOST_SPIRIT_DEFINE_ does.
-BOOST_SPIRIT_INSTANTIATE(start_decl, iterator_type, x3::unused_type);
+//BOOST_SPIRIT_INSTANTIATE(start_decl, iterator_type, x3::unused_type);
 
 #define EXPLICT_SPECIALIZATION_FROM_LINKER_ERROR_MSG
 #ifdef EXPLICT_SPECIALIZATION_FROM_LINKER_ERROR_MSG
