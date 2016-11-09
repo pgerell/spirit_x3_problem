61577be49c67b522bb32e2c8ac55483c1569aa08
Larry Evans <cppljevans@suddenlink.net>
2016-11-09 08:25:45 -0600

    links when
    defined(EXPLICIT_DEFINE_SPECIALIZATION_FROM_LINKER_ERROR_MSG):
    
    -*- mode: compilation; default-directory: "~/prog_dev/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/" -*-
    Compilation started at Wed Nov  9 08:24:51
    
    make -f ../linkerr.mk MAIN=prog run -Wcommon.cpp
    install -d `dirname /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o`
    /home/evansl/dwnlds/llvm/3.8/prebuilt/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04/bin/clang++ -c -O0 -stdlib=libc++  -std=c++14 -ftemplate-backtrace-limit=0 -fdiagnostics-show-template-tree -fno-elide-type -fmacro-backtrace-limit=0     -I/home/evansl/prog_dev/boost/releases/ro/boost_1_61_0    -ftemplate-depth=200  common.cpp -MMD -o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o
    sed -e 's#^common.o:#/tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o:#' /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.d> /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o.dep
    rm /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.d
    /home/evansl/dwnlds/llvm/3.8/prebuilt/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04/bin/clang++ -stdlib=libc++    /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.o  -o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.exe
    /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.exe
    
    Compilation finished at Wed Nov  9 08:24:53

--- common.cpp
+++ common.cpp
@@ -8,7 +8,12 @@ BOOST_SPIRIT_INSTANTIATE(start_decl, iterator_type, x3::unused_type);
 #ifdef EXPLICT_SPECIALIZATION_FROM_LINKER_ERROR_MSG
 template<>
 bool parse_rule<std::__1::__wrap_iter<char const*>, boost::spirit::x3::unused_type, ast::Start>(boost::spirit::x3::rule<start_class, ast::Start, false>, std::__1::__wrap_iter<char const*>&, std::__1::__wrap_iter<char const*> const&, boost::spirit::x3::unused_type const&, ast::Start&)
+#define EXPLICIT_DEFINE_SPECIALIZATION_FROM_LINKER_ERROR_MSG
+#ifndef EXPLICIT_DEFINE_SPECIALIZATION_FROM_LINKER_ERROR_MSG
 ;
+#else
+{ return false;}
+#endif
 //^Purpose:
 //  Simply copy&pasted name of undefined reference in
 //  the linker error message below
