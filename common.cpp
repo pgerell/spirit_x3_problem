0698402af0192ea9799440829cb72fd4c542d610
Larry Evans <cppljevans@suddenlink.net>
2016-11-09 17:02:06 -0600

    Attempt to force parse_rule instantiation fails:
    -*- mode: compilation; default-directory: "~/prog_dev/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/" -*-
    Compilation started at Wed Nov  9 17:00:08
    
    make -f ../linkerr.mk MAIN=prog x -Wcommon.cpp
    install -d `dirname /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o`
    /home/evansl/dwnlds/llvm/3.8/prebuilt/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04/bin/clang++ -c -O0 -stdlib=libc++  -std=c++14 -ftemplate-backtrace-limit=0 -fdiagnostics-show-template-tree -fno-elide-type -fmacro-backtrace-limit=0     -I/home/evansl/prog_dev/boost/releases/ro/boost_1_61_0    -ftemplate-depth=200  common.cpp -MMD -o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o
    sed -e 's#^common.o:#/tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o:#' /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.d> /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o.dep
    rm /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.d
    /home/evansl/dwnlds/llvm/3.8/prebuilt/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04/bin/clang++ -stdlib=libc++    /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.o  -o /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.exe
    /tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o: In function `bool boost::spirit::x3::rule<count_class, ast::Count, false>::parse<std::__1::__wrap_iter<char const*>, boost::spirit::x3::unused_type, ast::Start>(std::__1::__wrap_iter<char const*>&, std::__1::__wrap_iter<char const*> const&, boost::spirit::x3::unused_type const&, boost::spirit::x3::unused_type, ast::Start&) const':
    common.cpp:(.text._ZNK5boost6spirit2x34ruleI11count_classN3ast5CountELb0EE5parseINSt3__111__wrap_iterIPKcEENS1_11unused_typeENS4_5StartEEEbRT_RKSF_RKT0_SD_RT1_[_ZNK5boost6spirit2x34ruleI11count_classN3ast5CountELb0EE5parseINSt3__111__wrap_iterIPKcEENS1_11unused_typeENS4_5StartEEEbRT_RKSF_RKT0_SD_RT1_]+0x3c): undefined reference to `bool parse_rule<std::__1::__wrap_iter<char const*>, boost::spirit::x3::unused_type, ast::Start>(boost::spirit::x3::rule<count_class, ast::Count, false>, std::__1::__wrap_iter<char const*>&, std::__1::__wrap_iter<char const*> const&, boost::spirit::x3::unused_type const&, ast::Start&)'
    clang-3.8: error: linker command failed with exit code 1 (use -v to see invocation)
    /home/evansl/prog_dev/root.imk:199: recipe for target '/tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.exe' failed
    make: *** [/tmp/build/clangxx3_8_pkg/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.exe] Error 1
    
    Compilation exited abnormally with code 2 at Wed Nov  9 17:00:10

--- common.cpp
+++ common.cpp
@@ -1,11 +1,9 @@
 // The adapted include must be among the first (before any file that may include ast.h)
 #include "ast_adapted.hpp"
 #include "common_def.hpp"
-using start_decl=decltype(start);//mimic what BOOST_SPIRIT_DEFINE_ does.
-start_decl xxx=int(0);
-BOOST_SPIRIT_INSTANTIATE(start_decl, iterator_type, x3::unused_type);
+BOOST_SPIRIT_INSTANTIATE(start_type, iterator_type, x3::unused_type);
 
-#define EXPLICT_SPECIALIZATION_FROM_LINKER_ERROR_MSG
+//#define EXPLICT_SPECIALIZATION_FROM_LINKER_ERROR_MSG
 #ifdef EXPLICT_SPECIALIZATION_FROM_LINKER_ERROR_MSG
 template<>
 bool parse_rule<std::__1::__wrap_iter<char const*>, boost::spirit::x3::unused_type, ast::Start>(boost::spirit::x3::rule<start_class, ast::Start, false>, std::__1::__wrap_iter<char const*>&, std::__1::__wrap_iter<char const*> const&, boost::spirit::x3::unused_type const&, ast::Start&)
@@ -41,5 +39,16 @@ bool parse_rule<std::__1::__wrap_iter<char const*>, boost::spirit::x3::unused_ty
 
 
 const start_type& start_rule() {
+#if 1
+  //try to force instantiation of parse_rule.
+  //This mostly copied from prog.cpp:
+  std::string input="";
+  using iterator_type=std::string::const_iterator;
+  iterator_type iter = input.cbegin();
+  iterator_type end = input.cend();
+  x3::unused_type context;
+  ast::Start attr;
+  parse_rule(start,iter,end,context,attr);
+#endif  
   return start;
 }
