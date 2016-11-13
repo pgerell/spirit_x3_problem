9465fc9aa18416eba11026190b301e56d421c288
Larry Evans <cppljevans@suddenlink.net>
2016-11-13 09:09:22 -0600

    SHow workaround to linker problem:
    -*- mode: compilation; default-directory: "~/prog_dev/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/" -*-
    Compilation started at Sun Nov 13 09:03:25
    
    make -f ../linkerr.mk run
    install -d `dirname /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.o`
    /home/evansl/dwnlds/gcc/5.2.0/install/bin/g++ -c -O0    -O0 -gdwarf-2 -std=c++17 -Wall -Wno-deprecated -Wstrict-overflow -ftemplate-depth-200  -Wno-unused-local-typedefs -Wno-unused-but-set-variable  -I/home/evansl/prog_dev/boost/releases/ro/boost_1_61_0    -ftemplate-depth=200  expression.cpp -MMD -o /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.o
    In file included from ast_adapted.hpp:10:0,
                     from expression.cpp:2:
    ast.hpp:13:17: note: #pragma message: notdef(KEEP_CHAR)
     #pragma message "notdef(KEEP_CHAR)"
                     ^
    sed -e 's#^expression.o:#/tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.o:#' /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.d> /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.o.dep
    rm /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.d
    install -d `dirname /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/linker_err_msg.o`
    /home/evansl/dwnlds/gcc/5.2.0/install/bin/g++ -c -O0    -O0 -gdwarf-2 -std=c++17 -Wall -Wno-deprecated -Wstrict-overflow -ftemplate-depth-200  -Wno-unused-local-typedefs -Wno-unused-but-set-variable  -I/home/evansl/prog_dev/boost/releases/ro/boost_1_61_0    -ftemplate-depth=200  linker_err_msg.cpp -MMD -o /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/linker_err_msg.o
    sed -e 's#^linker_err_msg.o:#/tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/linker_err_msg.o:#' /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/linker_err_msg.d> /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/linker_err_msg.o.dep
    rm /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/linker_err_msg.d
    /home/evansl/dwnlds/gcc/5.2.0/install/bin/g++ -Wl,--library-path=/home/evansl/dwnlds/gcc/5.2.0/install/lib64 -Wl,-rpath=/home/evansl/dwnlds/gcc/5.2.0/install/lib64  /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.o /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/common.o /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/expression.o /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/linker_err_msg.o  -o /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.exe
    /tmp/build/gcc5_2_0/clang/dwnlds_work/dwnlds/Gerell.Boost_1_61_Spirit.LinkError.clone/cppljevans.boost_1_61_problem/prog.exe
    main:result=1:iter=

--- expression.cpp
+++ expression.cpp
@@ -3,3 +3,11 @@
 #include "expression_def.hpp"
 
 BOOST_SPIRIT_INSTANTIATE(count_type, iterator_type, x3::unused_type);
+
+#if 1
+//Workaround the link problem:
+    template bool parse_rule<iterator_type, x3::unused_type, ast::Start>( 
+        count_type rule_                                                         
+      , iterator_type& first, iterator_type const& last                          
+      , x3::unused_type const& context, ast::Start& attr);       
+#endif
